6a327b5312f30b64a540e9e4613b587db930c2d2
TahaU <taha.ugan55@gmail.com>
2024-07-18 11:31:35 +0300

    Changes to be committed: 	modified:   src/Models/User.py 	modified:   src/Models/__pycache__/BaseModel.cpython-312.pyc 	modified:   src/Models/__pycache__/Device.cpython-312.pyc 	modified:   src/Models/__pycache__/Log_In_Out.cpython-312.pyc 	modified:   src/Models/__pycache__/Loggers.cpython-312.pyc 	modified:   src/Models/__pycache__/Room.cpython-312.pyc 	modified:   src/Models/__pycache__/User.cpython-312.pyc

--- src/Models/User.py
+++ src/Models/User.py
@@ -29,7 +29,7 @@ class User(BaseModel):
             user_list = User.select()
         else:
             user_list = list(User.select().where(User.user_ID == id).dicts())
-            # print(user_list)
+            print(user_list)
             # print(User.ID)
         #
         # for user in users:
